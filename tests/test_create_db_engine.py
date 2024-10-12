d6e1d490f5eb17a86e9f15512db3c0c8def14816
Tomas Campi <totocampi@Tomass-Air.fibertel.com.ar>
2024-10-12 16:58:43 -0300

    Ajustes en paths

--- tests/test_create_db_engine.py
+++ tests/test_create_db_engine.py
@@ -6,17 +6,17 @@ import os
 
 # Add the 'scripts' directory to the Python path
 sys.path.append(os.path.join(os.path.dirname(__file__), '../airflow/scripts'))
-import crypto_app
+import utils
 
 class TestCreateDbEngine(unittest.TestCase):
     def setUp(self):
         self.mock_engine = MagicMock(spec=Engine)
 
-    @patch('crypto_app.create_engine')
-    @patch('crypto_app.get_env_variable')
+    @patch('etl.create_engine')
+    @patch('etl.get_env_variable')
     def test_create_db_engine(self, mock_get_env_variable, mock_create_engine):
         mock_get_env_variable.side_effect = ['user', 'pass', 'host', '5439', 'db']
-        crypto_app.create_db_engine()
+        utils.create_db_engine()
         mock_create_engine.assert_called_once_with("postgresql://user:pass@host:5439/db")
 
 if __name__ == '__main__':
--- tests/test_insert_crypto_data.py
+++ tests/test_insert_crypto_data.py
@@ -5,20 +5,20 @@ import os
 
 # Add the 'scripts' directory to the Python path
 sys.path.append(os.path.join(os.path.dirname(__file__), '../airflow/scripts'))
-import crypto_app
+import etl
 
 class TestInsertCryptoData(unittest.TestCase):
 
     def setUp(self):
         self.mock_engine = MagicMock()
 
-    @patch('crypto_app.get_crypto_symbols', return_value=['BTC', 'ETH'])  # Mock API response for symbols
-    @patch('crypto_app.get_crypto_price', side_effect=[('BTC', 50000.0), ('ETH', 4000.0)])  # Mock API price data
+    @patch('etl.get_crypto_symbols', return_value=['BTC', 'ETH'])  # Mock API response for symbols
+    @patch('etl.get_crypto_price', side_effect=[('BTC', 50000.0), ('ETH', 4000.0)])  # Mock API price data
     @patch.dict('os.environ', {'NINJA_API_KEY': 'fake_api_key'})  # Mock environment variable
     @patch('pandas.DataFrame.to_sql')  # Mock the to_sql method to avoid actual DB operations
     def test_insert_crypto_data(self, mock_to_sql, mock_get_price, mock_get_symbols):
         # Run the function
-        crypto_app.insert_crypto_data(self.mock_engine)
+        etl.insert_crypto_data(self.mock_engine)
         
         # Check if the to_sql method was called
         mock_to_sql.assert_called()  # This will ensure the to_sql method is called
