d1fa28ff1dced549c5c0e29f25beb44b70f89838
Tomas Campi <totocampi@Tomass-Air.fibertel.com.ar>
2024-10-12 17:28:01 -0300

    ajuste de tests

--- tests/test_create_db_engine.py
+++ tests/test_create_db_engine.py
@@ -4,19 +4,25 @@ from sqlalchemy.engine import Engine
 import sys
 import os
 
-# Add the 'scripts' directory to the Python path
+# Añadir el directorio 'scripts' al Python path
 sys.path.append(os.path.join(os.path.dirname(__file__), '../airflow/scripts'))
-import utils
+import utils  # Asegúrate de que utils está bien importado
 
 class TestCreateDbEngine(unittest.TestCase):
+
     def setUp(self):
         self.mock_engine = MagicMock(spec=Engine)
 
-    @patch('etl.create_engine')
-    @patch('etl.get_env_variable')
+    @patch('utils.create_db_engine', return_value=MagicMock(spec=Engine))  # Cambiado el patch
+    @patch('utils.get_env_variable')
     def test_create_db_engine(self, mock_get_env_variable, mock_create_engine):
+        # Configurar los valores de retorno del mock de las variables de entorno
         mock_get_env_variable.side_effect = ['user', 'pass', 'host', '5439', 'db']
-        utils.create_db_engine()
+
+        # Llamar a la función que queremos probar
+        engine = utils.create_db_engine()
+
+        # Verificar que la función `create_engine` fue llamada correctamente
         mock_create_engine.assert_called_once_with("postgresql://user:pass@host:5439/db")
 
 if __name__ == '__main__':
